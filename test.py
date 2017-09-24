6a92901a4a9274b912fa7a2c8917802385e6e7ad
cigrinho <amar.civgin@gmail.com>
2017-09-24 22:33:13 +0200

    small changes

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 from datetime import datetime
 from network import *
-from tqdm import tqdm
-from math import exp
 
 import loader
 
 start_time = datetime.now()
 
 cost = Cost.CrossEntropy
-learning_rate = .1
+learning_rate = .15
 batch_size = 6000
 hm_epoch = 100
-beta = 0.0002
+beta = 0.0003
 
 op_list = [('fc', (28 * 28, 70), Activation.Sigmoid),
            ('fl', (70, 10), Activation.Sigmoid, cost)]
@@ -37,15 +35,25 @@ def test():
         acc_cost = acc_cost + cost
         acc_results = acc_results + results
     print('Test dataset cost: {}'.format(acc_cost))
-    print('Test dataset accuracy: {}'.format(acc_results / (n * 5000)))
+    print('Test dataset accuracy: {}'.format(acc_results / 10000))
+    n = int(60000 / 6000)
+    current = 0
+    for i in range(n):
+        images, labels, current = loader.next_batch(batch_size=6000, current=current, dataset='training')
+        images = images.reshape(6000, -1, 1)
+        cost, _, accuracy, results, _, _ = net.run(ff_input=images, labels=labels)
+        acc_cost = acc_cost + cost
+        acc_results = acc_results + results
+    print('Test dataset cost: {}'.format(acc_cost))
+    print('Test dataset accuracy: {}'.format(acc_results /70000))
 
 
 def train():
-    w1 = np.loadtxt('data/w1', skiprows=1).reshape((-1, 4))
-    w2 = np.loadtxt('data/w2', skiprows=1).reshape((-1, 4))
-    b1 = np.loadtxt('data/b1', skiprows=1).reshape((-1, 4))
-    b2 = np.loadtxt('data/b2', skiprows=1).reshape((-1, 4))
-    cost_and_acc = np.loadtxt('data/cac', skiprows=1).reshape((-1, 3))
+    # w1 = np.loadtxt('data/w1', skiprows=1).reshape((-1, 4))
+    # w2 = np.loadtxt('data/w2', skiprows=1).reshape((-1, 4))
+    # b1 = np.loadtxt('data/b1', skiprows=1).reshape((-1, 4))
+    # b2 = np.loadtxt('data/b2', skiprows=1).reshape((-1, 4))
+    # cost_and_acc = np.loadtxt('data/cac', skiprows=1).reshape((-1, 3))
     for epoch in range(hm_epoch):
         current = 0
         acc_cost = 0
@@ -54,7 +62,7 @@ def train():
 
         n = int(60000/batch_size)
 
-        for _ in tqdm(range(n)):
+        for _ in range(n):
             images, labels, current = loader.next_batch(batch_size=batch_size, current=current)
             images = images.reshape(batch_size, -1, 1)
 
@@ -64,28 +72,28 @@ def train():
             acc_results = acc_results + results
 
             # BEGIN Save data
-            w1 = np.concatenate((w1, w_data[0].reshape(1, 4)))
-            w2 = np.concatenate((w2, w_data[1].reshape(1, 4)))
-
-            b1 = np.concatenate((b1, b_data[0].reshape(1, 4)))
-            b2 = np.concatenate((b2, b_data[1].reshape(1, 4)))
-
-            cost_and_acc = np.concatenate((cost_and_acc, np.array((cost, costL2, accuracy)).reshape(1, 3)))
+            # w1 = np.concatenate((w1, w_data[0].reshape(1, 4)))
+            # w2 = np.concatenate((w2, w_data[1].reshape(1, 4)))
+            #
+            # b1 = np.concatenate((b1, b_data[0].reshape(1, 4)))
+            # b2 = np.concatenate((b2, b_data[1].reshape(1, 4)))
+            #
+            # cost_and_acc = np.concatenate((cost_and_acc, np.array((cost, costL2, accuracy)).reshape(1, 3)))
             # END Save data
-        print('Epoch {} of {}, cost: {}, cost with L2: {}'.format(epoch,
-                                                                  hm_epoch,
-                                                                  round(acc_cost, 3),
-                                                                  round(acc_cost+acc_costL2, 3)),
+        print('Epoch {} of {}, cost: {}, L2 cost: {}'.format(epoch,
+                                                             hm_epoch,
+                                                             round(acc_cost, 3),
+                                                             round(acc_costL2, 3)),
               end=' ')
-        print('Accuracy: {}'.format(round(int(acc_results)/(n*batch_size), 3)))
-        net.save('saves/save'+str(epoch+164))
-        net.save('saves/save')
 
-        np.savetxt('data/w1',   w1,             fmt='%3.7f',    header='Weights 1')
-        np.savetxt('data/w2',   w2,             fmt='%3.7f',    header='Weights 2')
-        np.savetxt('data/b1',   b1,             fmt='%3.7f',    header='Biases 1')
-        np.savetxt('data/b2',   b2,             fmt='%3.7f',    header='Biases 2')
-        np.savetxt('data/cac',  cost_and_acc,   fmt='%3.7f',    header='Cost and Accuracy')
+        print('Accuracy: {}'.format(round(int(acc_results)/(n*batch_size), 5)))
+        net.save('saves/save')
+        net.save('saves/save{}'.format(epoch))
+        # np.savetxt('data/w1',   w1,             fmt='%3.7f',    header='Weights 1')
+        # np.savetxt('data/w2',   w2,             fmt='%3.7f',    header='Weights 2')
+        # np.savetxt('data/b1',   b1,             fmt='%3.7f',    header='Biases 1')
+        # np.savetxt('data/b2',   b2,             fmt='%3.7f',    header='Biases 2')
+        # np.savetxt('data/cac',  cost_and_acc,   fmt='%3.7f',    header='Cost and Accuracy')
 
     print("--- {} seconds ---".format(datetime.now() - start_time))
 
